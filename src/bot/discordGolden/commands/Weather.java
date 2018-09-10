7ce707d7049541dc2d8a5f71fb25cbeace07e626
Matheus Reis <mattreisro@gmail.com>
2018-09-09 22:21:07 -0300

    CODIGO FEIO

--- src/bot/discordGolden/commands/Weather.java
+++ src/bot/discordGolden/commands/Weather.java
@@ -24,35 +24,40 @@ public class Weather implements Command {
         OpenWeatherMapManager openWeatherManager = new OpenWeatherMapManager("c331c81f333e7a6e00a2ef6fcdd0d706");
         WeatherRequester weatherRequester = openWeatherManager.getWeatherRequester();
 
-        com.github.prominence.openweathermap.api.model.response.Weather weatherResponse = weatherRequester
-                .setLanguage(Language.ENGLISH)
-                .setUnitSystem(Unit.METRIC_SYSTEM)
-                .setAccuracy(Accuracy.ACCURATE)
-                .getByCityName(String.valueOf((args[1]).toUpperCase().contains(Cities.toUpperCase())));
+        String location = event.getMessage().getContentRaw().replace("!clima ", "").toUpperCase();
 
-        System.out.println((args.toString()).toUpperCase());
 
-        if(!String.valueOf(args.length).contains(Cities)) {
+        if (Cities.toUpperCase().contains(location.toUpperCase())) {
             event.getChannel().sendMessage("Coloque nome de uma cidade.").queue();
         } else {
-            EmbedBuilder clima = new EmbedBuilder();
-            clima.setColor(Color.CYAN);
-            clima.setAuthor(weatherResponse.getCityName() + ", " +weatherResponse.getCountry(), event.getAuthor().getAvatarUrl(),event.getAuthor().getAvatarUrl());
-            clima.setDescription("**Unidade:** " + weatherResponse.getTemperatureUnit() + "\n");
-            clima.appendDescription("**Latitude:** " + weatherResponse.getCoordinates().getLatitude() + "\n");
-            clima.appendDescription("**Longitude:** " + weatherResponse.getCoordinates().getLongitude() + "\n");
-            clima.appendDescription("\n");
-            clima.appendDescription("**Temp:** " + weatherResponse.getTemperature() + "\n");
-            if(weatherResponse.getRain() == null){
-                clima.appendDescription("**Chuva:** " + "Não está chovendo."+ "\n");
-            } else {
-                clima.appendDescription("**Chuva:** " + weatherResponse.getRain()+ "\n");
+            try {
+                com.github.prominence.openweathermap.api.model.response.Weather weatherResponse = weatherRequester
+                        .setLanguage(Language.ENGLISH)
+                        .setUnitSystem(Unit.METRIC_SYSTEM)
+                        .setAccuracy(Accuracy.ACCURATE)
+                        .getByCityName(location);
+
+                EmbedBuilder clima = new EmbedBuilder();
+                clima.setColor(Color.CYAN);
+                clima.setAuthor(weatherResponse.getCityName() + ", " + weatherResponse.getCountry(), event.getAuthor().getAvatarUrl(), event.getAuthor().getAvatarUrl());
+                clima.setDescription("**Unidade:** " + weatherResponse.getTemperatureUnit() + "\n");
+                clima.appendDescription("**Latitude:** " + weatherResponse.getCoordinates().getLatitude() + "\n");
+                clima.appendDescription("**Longitude:** " + weatherResponse.getCoordinates().getLongitude() + "\n");
+                clima.appendDescription("\n");
+                clima.appendDescription("**Temp:** " + weatherResponse.getTemperature() + "\n");
+                if (weatherResponse.getRain() == null) {
+                    clima.appendDescription("**Chuva:** " + "Não está chovendo." + "\n");
+                } else {
+                    clima.appendDescription("**Chuva:** " + weatherResponse.getRain() + "\n");
+                }
+                clima.appendDescription("**Vento:** " + weatherResponse.getWind() + "\n");
+                clima.appendDescription("\n");
+                clima.appendDescription("**Humidade:** " + weatherResponse.getHumidityPercentage() + " %" + "\n");
+                clima.appendDescription("****" + weatherResponse.getDataCalculationDate() + "\n");
+                event.getChannel().sendMessage(clima.build()).queue();
+            } catch (NullPointerException | com.github.prominence.openweathermap.api.exception.DataNotFoundException e) {
+                event.getChannel().sendMessage("Coloque nome de uma cidade valida.").queue();
             }
-            clima.appendDescription("**Vento:** " + weatherResponse.getWind()+ "\n");
-            clima.appendDescription("\n");
-            clima.appendDescription("**Humidade:** " + weatherResponse.getHumidityPercentage()+ " %" + "\n");
-            clima.appendDescription("****" + weatherResponse.getDataCalculationDate()+ "\n");
-            event.getChannel().sendMessage(clima.build()).queue();
 
         }
 
